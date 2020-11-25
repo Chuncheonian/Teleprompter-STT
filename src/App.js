3c14ea812892ea80561b2736c1700ba891d7713e
Chuncheonian <ehddud2468@khu.ac.kr>
2020-11-25 16:03:58 +0900

    Add event handling && Modify layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
-import { withStyles, Typography, Paper, Button} from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
+import { withStyles, Typography, Paper, Button, TextField} from '@material-ui/core';
 
 const styles = theme => ({
     root: {
@@ -12,44 +11,56 @@ const styles = theme => ({
       },
     },
     title: {
-        marginTop: 150,
+        marginTop: theme.spacing.unit * 5,
         textAlign: 'center',
         display: 'none',
-        fontSize: '3.0rem',
+        fontSize: '2.0rem',
         [theme.breakpoints.up('sm')]: {
           display: 'block',
-        //   fontSize: '20',
         }
     },
     paper: {
-        marginTop: 100,
-        marginLeft: 500,
-        marginRight: 500
+        marginTop: theme.spacing.unit * 5,
+        marginLeft: theme.spacing.unit * 20,
+        marginRight: theme.spacing.unit * 20
       },
     button: {
-        marginTop: 15,
-        marginLeft: 1320,
+        marginTop: theme.spacing.unit * 1,
+        marginLeft: theme.spacing.unit * 20,
+        fontSize: '1rem',
     },
   });
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            script: ''
+        }
+    }
+
+    // Get Script
+    getScript = (e) => {
+        this.script = e.target.value;
+    }
+
+    // Send script to server && Move Next URL
+    handleScriptSubmit = (e) => {
+        console.log(this.script);   // test
+    }
+
     render() {
         const { classes } = this.props;
         return (
-            <>
-                <Typography className={classes.title} variant="h6" color="inherit" noWrap>
-                음성에 맞춰 대본을 화면에 실시간으로 출력하는 프롬프터 서비스
-                </Typography>
+            <Fragment>
+                <Typography className={classes.title} variant="h6" color="inherit" noWrap>음성에 맞춰 대본을 화면에 실시간으로 출력하는 프롬프터 서비스</Typography>
                 <Paper className={classes.paper} elevation={3}>
-                    <TextField id="outlined-textarea" label="Script를 입력해주세요." fullWidth="true" rows={20} placeholder="Script" multiline variant="outlined"/>
+                    <TextField id="outlined-textarea" label="Script를 입력해주세요." fullWidth="true" rows={20} placeholder="Script" multiline variant="outlined" onChange={this.getScript}/>
                 </Paper>
-                <Button className={classes.button} variant="contained" color="primary">시작합니다.</Button>
-            </>
+                <Button className={classes.button} variant="contained" color="primary" size="medium" onClick={this.handleScriptSubmit}>시작합니다.</Button>
+            </Fragment>
         );
     }
 }
 
-// export default makeStyles(useStyles)(App);
-// export default App;
 export default withStyles(styles)(App);
-
